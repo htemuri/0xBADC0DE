0aac9ae938d37c8130ebf77ff60d2deb00925e33
Harris Temuri <harris.temuri@gmail.com>
2025-03-15 14:40:34 -0400

    ignore jekyll and add base path

--- next.config.mjs
+++ next.config.mjs
@@ -7,13 +7,14 @@ const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true // mandatory, otherwise won't export
-    }
+    },
+    basePath: "/0xBADC0DE",
     // Optional: Change the output directory `out` -> `dist`
     // distDir: "build"
 }
 const withNextra = nextra({
     // ... other Nextra config options
-    defaultShowCopyCode: true
+    defaultShowCopyCode: true,
 })
 
 export default withNextra(nextConfig)
